7a76c71e1b17d28e09e0d9a1256d5ce1142d4a8f
Natã <slipknotx1xbr@gmail.com>
2021-02-15 21:16:46 -0300

    Concluido Academia

---
+++ Twitch/academia/src/br/com/academia/App.java
@@ -0,0 +1,47 @@
+package br.com.academia;
+
+
+//import java.time.LocalDate;
+
+import br.com.academia.gui.FrameTela;
+//import br.com.academia.model.Aluno;
+
+public class App {
+ 
+	public static void main(String[] args) {
+			
+		FrameTela tela = new FrameTela();
+		tela.criarTela();
+		
+		// *** Nome *** \\
+		//Aluno nata = new Aluno();
+		//nata.setNome("Natã");
+		
+		// *** data de nascimento *** \\
+		//LocalDate dataNascimento = LocalDate.of(2004, 01, 25);
+		//nata.setDataNascimento(dataNascimento);
+		
+		// *** Altura *** \\
+		//nata.setAltura(1.78);
+		
+		// *** Peso *** \\
+		//nata.setPeso(100);
+		
+		// *** Sexo *** \\
+		//nata.setSexo("Masculino");
+		
+		// *** Nivel De Atividade *** \\
+		//nata.setNivelDeAtividade("Leve");
+		
+		//System.out.println(nata.getNome());
+		//System.out.println(nata.getDataNascimento());
+		//System.out.println(nata.getAltura());
+		//System.out.println(nata.getPeso());
+		//System.out.println(nata.getSexo());
+		//System.out.println(nata.getNivelDeAtividade());
+		//System.out.println(nata.getImc());
+		//System.out.println(nata.getStatusImc());
+		//System.out.println(nata.getNcd());
+	}
+	
+}
